267567a83fddc10f5712667c4750847038e96c39
Mridul-CODE05 <mridulbhardwaj888@gmail.com>
2026-06-15 23:09:19 +0530

    Implement player rotation and update logic

--- player.py
+++ player.py
@@ -1,12 +1,12 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH
+from constants import *
 import pygame
 
 class Player(CircleShape):
-    rotation = 0
     
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
 
 
     def triangle(self) -> list[pygame.Vector2]:
@@ -19,3 +19,15 @@ class Player(CircleShape):
     
     def draw(self, screen: pygame.Surface) -> None:
         pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH)
+
+    def rotate(self, dt: float):
+        self.rotation += PLAYER_TURN_SPEED * dt
+    
+    def update(self, dt: float) -> None:
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+
+        if keys[pygame.K_d]:
+            self.rotate(dt)
